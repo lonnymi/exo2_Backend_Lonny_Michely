1ac61300c97bb77bd0fc94e69356378d3009e4e0
TonNom <ton.email@example.com>
2025-10-16 14:15:27 +0200

    implement in-memory list/add/delete with auto-increment id

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -11,13 +11,12 @@ class TaskController {
     return this.tasks;
   }
 
-    addTask(title) {
-      const task = new Task(this.nextId, title, false); // id puis title
-      this.tasks.push(task);
-      this.nextId += 1;
-      return task;
-    }
-
+  addTask(title) {
+    const task = new Task(this.nextId, title, false); // id puis title
+    this.tasks.push(task);
+    this.nextId += 1;
+    return task;
+  }
 
   deleteTask(id) {
     const idx = this.tasks.findIndex(t => t.id === id);
